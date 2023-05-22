177fc7a96d3df1d2cfc495ad2a0017e25158364f
Ilyes1 <ihannouch7@gmail.com>
2023-05-22 11:04:54 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const EventController = require('./controllers/eventController');
-const UserController = require('./controllers/userController');
+const UserController = require('./functions/userController');
 // const userRoutes = require('./routes/user');
 const cors = require('cors')
 
